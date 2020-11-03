fe929833cfba19cb67e6a87ef8a1ce078ee9879e
Muchun Song <songmuchun@bytedance.com>
2020-11-03 19:14:10 +0800

    bytedance: mm: memcontrol: Add watermark event
    When the free memory hit the low watermark, we will notify the userland.
    
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2510,9 +2510,6 @@ static void memcg_reclaim_work(struct work_struct *work)
 	unsigned long low, high, free;
 	unsigned long nr_reclaimed;
 
-	if (memcg->reclaim_failures >= MEMCG_RECLAIM_RETRY)
-		return;
-
 	memcg_wmark_lock(memcg);
 	low = memcg_low_wmark_pages(memcg);
 	high = memcg_high_wmark_pages(memcg);
@@ -2520,6 +2517,11 @@ static void memcg_reclaim_work(struct work_struct *work)
 	memcg_wmark_unlock(memcg);
 
 	if (free >= low)
+		return;
+
+	cgroup_file_notify(&memcg->wmark_low_event);
+
+	if (memcg->reclaim_failures >= MEMCG_RECLAIM_RETRY)
 		return;
 
 	nr_reclaimed = try_to_free_mem_cgroup_pages_asyn(memcg, high - free,
@@ -5293,6 +5295,7 @@ static struct cftype mem_cgroup_legacy_files[] = {
 	},
 	{
 		.name = "watermark",
+		.file_offset = offsetof(struct mem_cgroup, wmark_low_event),
 		.seq_show = memory_wmark_read,
 	},
 #endif
@@ -6762,6 +6765,7 @@ static struct cftype memory_files[] = {
 	{
 		.name = "watermark",
 		.flags = CFTYPE_NOT_ON_ROOT,
+		.file_offset = offsetof(struct mem_cgroup, wmark_low_event),
 		.seq_show = memory_wmark_read,
 	},
 #endif
